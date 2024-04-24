3a06f4025b22273bd84fd746fcb5324fb1c6b497
Kamronbek03 <kamronb3k03@gmail.com>
2024-04-25 00:14:25 +0500

    homework

---
+++ js/main.js
@@ -0,0 +1,145 @@
+// Uyha vazifa ////////////////////
+// Boolean 1
+// let a;
+// a = +prompt("a=");
+// console.log(a % 2 == 0);
+
+// Boolean 2
+// let A;
+// let B;
+// A = +prompt("A=");
+// B = +prompt("B=");
+// console.log(A >= 0 || B < -2);
+
+// Boolean 3
+// let A;
+// let B;
+// let C;
+// A = +prompt("A=");
+// B = +prompt("B=");
+// C = +prompt("C=");
+// console.log((A < B && B < C) || (C < B && B < A));
+
+// Boolean 4
+// let A;
+// let B;
+// A = +prompt("A=");
+// B = +prompt("B=");
+// console.log(A % 2 != 0 && B % 2 != 0);
+
+// Boolean 5
+// let A;
+// let B;
+// A = +prompt("A=");
+// B = +prompt("B=");
+// console.log(A % 2 != 0 || B % 2 != 0);
+
+// Boolean 6
+// let A;
+// let B;
+// A = +prompt("A=");
+// B = +prompt("B=");
+// console.log((A % 2 == 0 && B % 2 != 0) || (A % 2 != 0 && B % 2 == 0));
+
+// Boolean 7
+// let A;
+// let B;
+// let C;
+// A = +prompt("A=");
+// B = +prompt("B=");
+// C = +prompt("C=");
+// console.log(A > 0 && B > 0 && C > 0);
+
+// Boolean 8
+// let A;
+// let B;
+// let C;
+// A = +prompt("A=");
+// B = +prompt("B=");
+// C = +prompt("C=");
+// console.log((A > 0 && B < 0 && C < 0) || (A < 0 && B > 0 && C < 0) || (A < 0 && B < 0 && C > 0));
+
+// Boolean 9
+// let A;
+// let B;
+// let C;
+// A = +prompt("A=");
+// B = +prompt("B=");
+// C = +prompt("C=");
+// console.log((A > 0 && B > 0 && C < 0) || (A > 0 && B < 0 && C > 0) || (A < 0 && B > 0 && C > 0));
+
+// Boolean 10
+// let a;
+// a = +prompt("a=");
+// console.log(a > 0 && a >= 10 && a < 100 && a % 2 == 0);
+
+// Boolean 11
+// let a;
+// a = +prompt("a=");
+// console.log(a > 0 && a >= 100 && a < 1000 && a % 2 != 0);
+
+// Boolean 12
+// let A;
+// let B;
+// let C;
+// A = +prompt("A=");
+// B = +prompt("B=");
+// C = +prompt("C=");
+// console.log ( A + B == 0 || A + C == 0 || B + C == 0)
+
+// Boolean 13
+// let a;
+// a = +prompt("Uch xonali son kiriting !");
+// let d = a % 10;
+// let c = ((a % 100) - d) / 10;
+// let b = (a - (a % 100)) / 100;
+// console.log(b < c && c < d);
+
+// Boolean 14
+// let a;
+// a = +prompt("Uch xonali son kiriting !");
+// let d = a % 10;
+// let c = ((a % 100) - d) / 10;
+// let b = (a - (a % 100)) / 100;
+// console.log((b < c && c < d) || (b > c && c > d));
+
+// Boolean 15
+// let a;
+// a = +prompt("Uch xonali son kiriting !");
+// let d = a % 10;
+// let c = ((a % 100) - d) / 10;
+// let b = (a - (a % 100)) / 100;
+// console.log(b == d);
+
+// Boolean 16
+// let x1;
+// let y1;
+// let x2;
+// let y2;
+// x1 = +prompt("x1=");
+// y1 = +prompt("y1=");
+// x2 = +prompt("x2=");
+// y2 = +prompt("y2=");
+// console.log(((x1 % 2 == 0 && x2 % 2 == 0) || (x1 % 2 != 0 && x2 % 2 != 0)) && ((y1 % 2 == 0 && y2 % 2 == 0) || (y1 % 2 != 0 && y2 % 2 != 0)));
+
+// Boolean 17
+// let x1;
+// let y1;
+// let x2;
+// let y2;
+// x1 = +prompt("x1=");
+// y1 = +prompt("y1=");
+// x2 = +prompt("x2=");
+// y2 = +prompt("y2=");
+// console.log(x1 == x2 || y1 == y2);
+
+// Boolean 19
+// let x1;
+// let y1;
+// let x2;
+// let y2;
+// x1 = +prompt("x1=");
+// y1 = +prompt("y1=");
+// x2 = +prompt("x2=");
+// y2 = +prompt("y2=");
+// console.log(x1 - x2 == y1 - y2 || x2 - x1 == y2 - y1);
